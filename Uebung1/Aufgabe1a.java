87f631701dfafad0f0c3f5b4fe160e45339bd6f2
Christian <c.stegmaier@htwg-konstanz.de>
2023-10-11 15:12:38 +0200

    erste Codeschnipsel

--- Uebung1/Aufgabe1a.java
+++ Uebung1/Aufgabe1a.java
@@ -9,7 +9,8 @@ public class Aufgabe1a {
 
 
     public static void main(String[] args) {
-        String[] strings = new String[]{input1, input2, input3, input4, input5};
+        //String[] strings = new String[]{input1, input2, input3, input4, input5};
+        String[] strings = new String[]{"grueziWohl!", "hallo%boi"};
 
         parseAllStrings(strings);
     }
@@ -19,6 +20,8 @@ public class Aufgabe1a {
         for (String s : strings
              ) {
             if (parseOneString(s) == false) {
+                System.err.println("Der String " + s + " + konnte nicht korrekt geparst werden. " +
+                        "Eventuell ist der Format-String nicht korrekt.");
                 return false;
             }
         }
@@ -29,13 +32,28 @@ public class Aufgabe1a {
 
     private static boolean parseOneString(String s) {
 
+        System.out.println("-------- parsing String " +s+ "--------");
+
         //laufe den String zeichenweise ab, ob ein Prozentzeichen vorkommt
+        int posOfPercentCypher = s.indexOf("%");
 
         //falls nein, alles nur Text
+        if (posOfPercentCypher == -1) {
+            System.out.println("(es gibt keine Prozentzeichen im String " + s + ")");
+            System.out.println(returnAsText(s));
+            return true;
+        }
+
+
 
         return false;
     }
 
+
+    private static String returnAsText(String s) {
+        return "TEXT(\"" + s + "\")";
+    }
+
+
+
 }
-
-
