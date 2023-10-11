b4ee9b0786feda0b292c74118be2ae433c1c5566
Christian <c.stegmaier@htwg-konstanz.de>
2023-10-11 12:18:03 +0200

    erstes Codegerüst

---
+++ Uebung1/Aufgabe1a.java
@@ -0,0 +1,35 @@
+public class Aufgabe1a {
+
+    static String input1 = "xxx %d yyy%n";
+    static String input2 = "xxx% 012d yyy%%";
+    static String input3 = "xxx%1$d yyy";
+    static String input4 = "%1$0+(32.10fyyy";
+    static String input5 = "Wochentag: %tA Uhrzeit: %tT";
+
+
+
+    public static void main(String[] args) {
+        String[] strings = new String[]{input1, input2, input3, input4, input5};
+
+        parseAllStrings(strings);
+    }
+
+    private static boolean parseAllStrings(String[] strings) {
+
+        for (String s : strings
+             ) {
+            if (parseOneString(s) == false) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    private static boolean parseOneString(String s) {
+
+        return false;
+    }
+
+}
--- Uebung1/a1.java
+++ Uebung1/a1.java
@@ -1,7 +1,7 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Main {
+public class a1 {
     public static void main(String[] args) {
         Pattern pattern = Pattern.compile("w3schools", Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher("Visit W3Schools!");
