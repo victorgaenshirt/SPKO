f02fa3a62e10268ea41929bd587eb9101d2aebd8
victorgaenshirt <vgaenshirt@posteo.de>
2023-10-17 16:31:45 +0200

    works and finds the pattern matching parts of the string, but has no prints yet

--- Uebung1/Aufgabe1a.java
+++ Uebung1/Aufgabe1a.java
@@ -1,41 +1,32 @@
-public class Aufgabe1a {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class Aufgabe1a {
     static String input1 = "xxx %d yyy%n";
     static String input2 = "xxx% 012d yyy%%";
     static String input3 = "xxx%1$d yyy";
     static String input4 = "%1$0+(32.10fyyy";
     static String input5 = "Wochentag: %tA Uhrzeit: %tT";
 
-
-
     public static void main(String[] args) {
-        String[] strings = new String[]{input1, input2, input3, input4, input5};
-
-        parseAllStrings(strings);
+        parse(input1);
+        parse(input2);
+        parse(input3);
+        parse(input4);
+        parse(input5);
     }
 
-    private static boolean parseAllStrings(String[] strings) {
-
-        for (String s : strings
-             ) {
-            if (parseOneString(s) == false) {
-                return false;
+    private static void parse(String input) {
+            Pattern pattern = Pattern.compile("(%(\\d+\\$)?([-#+ 0,(<]*)?(\\d+)?(\\.\\d+)?[tT]?[a-zA-Z%])");
+            Matcher matcher = pattern.matcher(input);
+            while (matcher.find()) {
+                int start = matcher.start();
+                int end = matcher.end();
+                String match = input.substring(start, end);
+                System.out.println(match);
+                String formatSpecifier = matcher.group();
             }
-        }
-
-        return true;
     }
-
-
-    private static boolean parseOneString(String s) {
-
-        //laufe den String zeichenweise ab, ob ein Prozentzeichen vorkommt
-
-        //falls nein, alles nur Text
-
-        return false;
-    }
-
 }
 
 
