1d1271cd2147e6cd3c846e42384ec8e314481b60
victorgaenshirt <vgaenshirt@posteo.de>
2024-01-17 15:20:38 +0100

    aufgabe6 refactor

--- Uebung6/aufgabe6.java
+++ Uebung6/aufgabe6.java
@@ -1,44 +1,21 @@
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.Collection;
+package Uebung6;
+
 import org.stringtemplate.v4.*;
-public class aufgabe6 {
+public final class aufgabe6 {
+    private aufgabe6() { }
 
-    public static void main(String[] args) {
-        if (args.length == 0) {
-            System.exit(1);
-        }
+    public static void main(String[] args) throws Exception {
+        Class<?> cl1 = Class.forName("java.lang.String");
+        Class<?> cl2 = Class.forName("java.lang.Iterator");
+        Class<?> cl3 = Class.forName("java.time.Month");
 
-        Collection<Class<?>> classes = Arrays.asList(args).stream()
-                .map(aufgabe6::loadClass)
-                .toArray(Class[]::new);
+        Class<?> arr[] = {cl1, cl2, cl3};
 
-        generateHTML(classes);
-    }
-
-    private static Class<?> loadClass(String className) {
-        try {
-            return Class.forName(className);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException("Class not found: " + className, e);
-        }
-    }
-
-    private static void generateHTML(Collection<Class<?>> classes) {
-        ST rootTemplate = new ST("<!DOCTYPE html>\n<html lang=\"de\">\n<head>\n<style type=\"text/css\">\nth, td { border-bottom: thin solid; padding: 4px; text-align: left; }\ntd { font-family: monospace }\n</style>\n</head>\n<body>\n<h1>Sprachkonzepte, Aufgabe 6</h1>\n<classes(classes)>\n</body>\n</html>");
-
-        rootTemplate.add("classes", classes);
-        for (Class<?> clazz : classes) {
-            ST classTemplate = new ST("<classDecl(clazz)>");
-            classTemplate.add("clazz", clazz);
-            rootTemplate.add("classes", classTemplate);
-            for (Class<?> iface : clazz.getInterfaces()) {
-                ST ifaceTemplate = new ST("<interfaceDecl(iface)>");
-                ifaceTemplate.add("iface", iface);
-                rootTemplate.add("classes", ifaceTemplate);
-            }
-        }
-
-        System.out.println(rootTemplate.render());
+        ST templ = new STGroupFile("aufgabe.stg").getInstanceOf("root");
+        templ.add("n", arr);
+        String page = templ.render();
+        System.out.println(page);
     }
 }
+
+//How to compile: javac -classpath antlr-4.9.2-complete.jar aufgabe6.java
